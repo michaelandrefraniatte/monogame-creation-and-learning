4580eb824927a1562fe750d769baa88b23a28b24
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-19 20:08:08 +0200

    Add a program

---
+++ Src/RenderFormTest/RenderFormTest/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System.Drawing.Imaging;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using AlphaMode = SharpDX.Direct2D1.AlphaMode;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+using Rectangle = System.Drawing.Rectangle;
+using Bitmap = System.Drawing.Bitmap;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.Windows;
+using SharpDX.Direct3D;
+using SharpDX.D3DCompiler;
+using DXMath = SharpDX.Mathematics.Interop;
+using D3D11 = SharpDX.Direct3D11;
+namespace RenderFormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        public static uint CurrentResolution = 0;
+        private static WindowRenderTarget target;
+        private static SharpDX.Direct2D1.Factory1 fact = new SharpDX.Direct2D1.Factory1();
+        private static RenderTargetProperties renderProp;
+        private static HwndRenderTargetProperties winProp;
+        private static bool closed = false;
+        private static int width, height;
+        private static System.Drawing.Bitmap EditableImg;
+        private RenderForm renderForm;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+            EditableImg = System.Drawing.Bitmap.FromFile("image.png") as System.Drawing.Bitmap;
+            width = EditableImg.Width;
+            height = EditableImg.Height;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            renderForm = new RenderForm(textBox1.Text);
+            renderForm.ClientSize = new Size(width, height);
+            renderForm.AllowUserResizing = false;
+            InitDisplayCapture(renderForm.Handle);
+            Task.Run(() => Start());
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            closed = true;
+        }
+        private static void Start()
+        {
+            closed = false;
+            while (!closed)
+            {
+                DisplayCapture(EditableImg);
+                System.Threading.Thread.Sleep(1);
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            closed = true;
+        }
+        private static void InitDisplayCapture(IntPtr handle)
+        {
+            renderProp = new RenderTargetProperties()
+            {
+                DpiX = 0,
+                DpiY = 0,
+                MinLevel = SharpDX.Direct2D1.FeatureLevel.Level_DEFAULT,
+                PixelFormat = new SharpDX.Direct2D1.PixelFormat(Format.B8G8R8A8_UNorm, AlphaMode.Premultiplied),
+                Type = RenderTargetType.Hardware,
+                Usage = RenderTargetUsage.None
+            };
+            winProp = new HwndRenderTargetProperties()
+            {
+                Hwnd = handle,
+                PixelSize = new Size2(width, height),
+                PresentOptions = PresentOptions.Immediately
+            };
+            target = new WindowRenderTarget(fact, renderProp, winProp);
+        }
+        private static void DisplayCapture(Bitmap image1)
+        {
+            using (var bmp = image1)
+            {
+                System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+                SharpDX.DataStream stream = new SharpDX.DataStream(bmpData.Scan0, bmpData.Stride * bmpData.Height, true, false);
+                SharpDX.Direct2D1.PixelFormat pFormat = new SharpDX.Direct2D1.PixelFormat(SharpDX.DXGI.Format.B8G8R8A8_UNorm, AlphaMode.Premultiplied);
+                SharpDX.Direct2D1.BitmapProperties bmpProps = new SharpDX.Direct2D1.BitmapProperties(pFormat);
+                SharpDX.Direct2D1.Bitmap result = new SharpDX.Direct2D1.Bitmap(target, new SharpDX.Size2(width, height), stream, bmpData.Stride, bmpProps);
+                bmp.UnlockBits(bmpData);
+                stream.Dispose();
+                bmp.Dispose();
+                target.BeginDraw();
+                target.Clear(new SharpDX.Mathematics.Interop.RawColor4(0, 0, 0, 1f));
+                target.DrawBitmap(result, 1.0f, BitmapInterpolationMode.NearestNeighbor);
+                target.EndDraw();
+            }
+        }
+    }
+}
